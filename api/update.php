604bacc1036de80995f83ed6cfc9c47d2357b00e
Jasom Rocha <36940932+JasomRocha@users.noreply.github.com>
2025-10-22 22:51:36 -0300

    Cria estrutura do update

--- api/update.php
+++ api/update.php
@@ -1,2 +1,15 @@
-// terminar amanhã...
+<?php
+require "./config.php";
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+
+// o modelo do bady deve ser x-www-form-urlencoded ele nao reconhece JSON por padrao
+if ($method === 'put') {
+
+
+}else{
+    $array['error'] = [ 'Method not allowed'];
+}
+
+require "./return.php";
 
